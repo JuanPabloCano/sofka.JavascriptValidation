81c19b0fefdfe98e139a5888886957fd1934fe97
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-19 13:50:30 -0500

    Desarrollo de método de colisión de barras superiores e inferiores, implementación de comentarios y modificación de velocidad de barras y pelota

--- PingPong/main.js
+++ PingPong/main.js
@@ -17,18 +17,19 @@
 	}
 })();
 
+// Función para crear la pelota
 (function () {
 	self.Ball = function (x, y, radius, board) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
-		this.board = board;
 		this.speed_y = 0;
 		this.speed_x = 3;
+		this.board = board;
 		this.direction = 1;
 		this.bounce_angle = 0;
 		this.max_bounce_angle = Math.PI / 12;
-		this.speed = 3;
+		this.speed = 8;
 		board.ball = this;
 		this.kind = "circle";
 	}
@@ -36,18 +37,42 @@
 		move: function () {
 			this.x += (this.speed_x * this.direction);
 			this.y += (this.speed_y);
-		},
 
-		get width(){
+			if (this.x <= 10) {
+				this.x = 400;
+				this.y = 200;
+				this.speed_x = -this.speed_x;
+				this.bounce_angle = -this.bounce_angle;
+			}
+			if (this.x >= 790) {
+				this.x = 400;
+				this.y = 200;
+				this.speed_x = -this.speed_x;
+				this.bounce_angle = -this.bounce_angle;
+			}
+
+			// Controlador de los impactos 
+			if (this.y <= 10) {
+				this.speed_y = -this.speed_y;
+				this.bounce_angle = -this.bounce_angle;
+			}
+			if (this.y >= 390) {
+				this.speed_y = -this.speed_y;
+				this.bounce_angle = -this.bounce_angle;
+			}
+		},
+		get width() {
 			return this.radius * 2;
 		},
-		get height(){
+		get height() {
 			return this.radius * 2;
 		},
-		collision: function(bar){
-			var relative_intersect_y = (bar.y + (bar.height / 2) - this.y);
-			var normalizaed_intersect_y = relative_intersect_y / (bar.height / 2);
-			this.bounce_angle = normalizaed_intersect_y * this.max_bounce_angle;
+		collision: function (bar) {
+			// Función para reaccionar a las colisiones con las barras laterales
+			var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+			var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+			this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
 			this.speed_y = this.speed * -Math.sin(this.bounce_angle);
 			this.speed_x = this.speed * Math.cos(this.bounce_angle);
 
@@ -57,7 +82,7 @@
 	}
 })();
 
-
+// Función que define el tamaño del tablero
 (function () {
 	self.Bar = function (x, y, width, height, board) {
 		this.x = x;
@@ -65,9 +90,9 @@
 		this.width = width;
 		this.height = height;
 		this.board = board;
-		this.board.bars.push(this);
-		this.kind = 'rectangle';
-		this.speed = 15;
+		this.board.bars.push(this); // Agregar barras al tablero
+		this.kind = 'rectangle'; // Forma geométrica de las barras
+		this.speed = 20; // Velocidad de la barra
 	}
 	self.Bar.prototype = {
 		down: function () {
@@ -82,13 +107,14 @@
 	}
 })();
 
+// Función encargada de dibujar el tablero de juego
 (function () {
 	self.BoardView = function (canvas, board) {
 		this.canvas = canvas;
 		this.canvas.width = board.width;
 		this.canvas.height = board.height;
 		this.board = board;
-		this.ctx = this.canvas.getContext('2d');
+		this.ctx = this.canvas.getContext('2d'); 
 	}
 
 	self.BoardView.prototype = {
@@ -98,29 +124,30 @@
 		draw: function () {
 			for (var i = this.board.elements.length - 1; i >= 0; i--) {
 				var el = this.board.elements[i];
-				this.ctx.fillStyle = "white";
+
 				draw(this.ctx, el);
 			}
 		},
-		check_collisions: function(){
-			for (let i = this.board.bars.length -1; i >= 0; i--) {
+		// Función para comprobar las colisiones
+		check_collisions: function () {
+			for (var i = this.board.bars.length - 1; i >= 0; i--) {
 				var bar = this.board.bars[i];
-				if (hit(bar, this.board.ball))
-				this.board.ball.collision(bar);
+				if (hit(bar, this.board.ball)) {
+					this.board.ball.collision(bar);
+				}
 			}
 		},
-
 		play: function () {
-			if (this.board.playing){
-				this.clean();
-				this.draw();
-				this.check_collisions();
-				this.board.ball.move();
+			if (!this.board.playing) {
+				this.clean(); // Limpia el tablero
+				this.draw(); // Dibuja el tablero
+				this.check_collisions(); // Verifica las colisiones
+				this.board.ball.move(); // Mueve la pelota
 			}
 		}
 	}
 
-	function hit(a, b){
+	function hit(a, b) {
 		var hit = false;
 		if (b.x + b.width >= a.x && b.x < a.x + a.width) {
 			if (b.y + b.height >= a.y && b.y < a.y + a.height)
@@ -152,42 +179,50 @@
 	}
 })();
 
-var board = new Board(800, 400);
-var bar = new Bar(20, 150, 20, 100, board);
-var bar_2 = new Bar(735, 150, 20, 100, board);
-var canvas = document.getElementById('canvas');
-var board_view = new BoardView(canvas, board);
-var ball = new Ball(350, 100, 10, board);
-
+var board = new Board(800, 400); // Se crea el tablero con las dimensiones establecidas en los argumentos
+var bar = new Bar(0, 150, 20, 100, board); // Creación de la barra izquierda
+var bar_2 = new Bar(780, 150, 20, 100, board); // Creación de la barra derecha
+var canvas = document.getElementById('canvas'); // Obtención del tablero
+var board_view = new BoardView(canvas, board); // Creación del tablero
+var ball = new Ball(400, 200, 10, board); // Creación de la pelota
 
 document.addEventListener("keydown", function (ev) {
-	ev.preventDefault();
+	if (ev.keyCode == 38) {
+		ev.preventDefault();
+		if (bar_2.y >= 10) {
+			bar_2.up(); // Mueve barra derecha hacia arriba
+		}
+	}
+	else if (ev.keyCode == 40) {
+		ev.preventDefault();
+		if (bar_2.y <= 290) {
+			bar_2.down(); // Mueve la barra derecha hacia abajo
+		}
+	}
+	else if (ev.keyCode == 87) {
+		//W
+		ev.preventDefault();
+		if (bar.y >= 10) {
+			bar.up(); // Mueve la barra izquierda hacia arriba
+		}
 
-	if (ev.keyCode === 38) {
-		ev.preventDefault();
-		bar.up();
 	}
-	else if (ev.keyCode === 40) {
+	else if (ev.keyCode == 83) {
+		//S
 		ev.preventDefault();
-		bar.down();
-	}
-	else if (ev.keyCode === 87) {
-		ev.preventDefault();
-		bar_2.up();
-	}
-	else if (ev.keyCode === 83) {
-		ev.preventDefault();
-		bar_2.down();
-	}
-	else if (ev.keyCode === 32){
+		if (bar.y <= 290) {
+			bar.down(); // Mueve la barra izquierda hacia abajo
+		}
+	} else if (ev.keyCode == 32) {
 		ev.preventDefault();
 		board.playing = !board.playing;
 	}
 });
 
-board_view.draw();
-window.requestAnimationFrame(controller);
+board_view.draw(); // Dibuja el tablero al inicio
+window.requestAnimationFrame(controller); 
 
+// Función para iniciar el juego
 function controller() {
 	board_view.play();
 	window.requestAnimationFrame(controller);
