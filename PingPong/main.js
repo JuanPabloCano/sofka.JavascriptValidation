8fb824a2b21af31da8787ede471391a517bb540f
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-19 12:36:37 -0500

    Modificación de error en la pelota y su velocidad, creación de KeyWord space para detener e iniciar el juego

--- PingPong/main.js
+++ PingPong/main.js
@@ -17,18 +17,24 @@
 	}
 })();
 
-(function (){
-    self.Ball = function(x, y, radius, board){
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.board = board;
-        this.speed_y = 0;
-        this.speed_x = 3;
-        board.ball = this;
-        this.kind = "circle";
-    }
-
+(function () {
+	self.Ball = function (x, y, radius, board) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.board = board;
+		this.speed_y = 0;
+		this.speed_x = 3;
+		this.direction = 1;
+		board.ball = this;
+		this.kind = "circle";
+	}
+	self.ball.prototype = {
+		move: function () {
+			this.x += (this.speed_x * this.direction);
+			this.y += (this.speed_y);
+		}
+	}
 })();
 
 
@@ -39,9 +45,9 @@
 		this.width = width;
 		this.height = height;
 		this.board = board;
-		this.board.bars.push(this); 
-		this.kind = 'rectangle'; 
-		this.speed = 15; 
+		this.board.bars.push(this);
+		this.kind = 'rectangle';
+		this.speed = 15;
 	}
 	self.Bar.prototype = {
 		down: function () {
@@ -61,26 +67,29 @@
 		this.canvas = canvas;
 		this.canvas.width = board.width;
 		this.canvas.height = board.height;
-		this.board = board; 
-		this.ctx = this.canvas.getContext('2d'); 
+		this.board = board;
+		this.ctx = this.canvas.getContext('2d');
 	}
 
 	self.BoardView.prototype = {
-        clean: function(){
-            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
-        },
+		clean: function () {
+			this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+		},
 		draw: function () {
-			for (var i = this.board.elements.length - 1; i >= 0; i--) { 
+			for (var i = this.board.elements.length - 1; i >= 0; i--) {
 				var el = this.board.elements[i];
 				this.ctx.fillStyle = "white";
 				draw(this.ctx, el);
 			}
 		},
-        play: function(){
-            this.clean();
-            this.draw();        
-        }
-    }
+		play: function () {
+			if (this.board.playing){
+				this.clean();
+				this.draw();
+				this.board.ball.move();
+			}
+		}
+	}
 	function draw(ctx, element) {
 		switch (element.kind) {
 			case 'rectangle':
@@ -97,34 +106,42 @@
 })();
 
 var board = new Board(800, 400);
-var bar = new Bar(20, 150, 20, 100, board); 
+var bar = new Bar(20, 150, 20, 100, board);
 var bar_2 = new Bar(735, 150, 20, 100, board);
 var canvas = document.getElementById('canvas');
-var board_view = new BoardView(canvas, board); 
+var board_view = new BoardView(canvas, board);
 var ball = new Ball(350, 100, 10, board);
 
 
-document.addEventListener("keydown", function(ev){
-    ev.preventDefault();
+document.addEventListener("keydown", function (ev) {
+	ev.preventDefault();
 
-    if (ev.keyCode == 38){
-        bar.up();
-    }
-    else if (ev.keyCode == 40){
-        bar.down();
-    }
-    else if (ev.keyCode == 87){
-        bar_2.up();
-    }
-    else if (ev.keyCode == 83){
-        bar_2.down();
-    }
+	if (ev.keyCode === 38) {
+		ev.preventDefault();
+		bar.up();
+	}
+	else if (ev.keyCode === 40) {
+		ev.preventDefault();
+		bar.down();
+	}
+	else if (ev.keyCode === 87) {
+		ev.preventDefault();
+		bar_2.up();
+	}
+	else if (ev.keyCode === 83) {
+		ev.preventDefault();
+		bar_2.down();
+	}
+	else if (ev.keyCode === 32){
+		ev.preventDefault();
+		board.playing = !board.playing;
+	}
 });
 
+board_view.draw();
 window.requestAnimationFrame(controller);
 
-
-function controller(){
-    board_view.play();
-    window.requestAnimationFrame(controller);
+function controller() {
+	board_view.play();
+	window.requestAnimationFrame(controller);
 }
